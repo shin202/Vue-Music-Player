02f6b6a302cf304d07327ba85b2443b3ceddd37e
Tran Viet Hoang <tranviethoang642@gmail.com>
2022-07-12 14:51:30 +0700

    fix icon missing

--- src/components/SongsListItem.vue
+++ src/components/SongsListItem.vue
@@ -1,17 +1,16 @@
 <template>
     <li class="songs__item sticky bottom-0 flex justify-around items-center gap-[1rem] bg-white m-4 p-4 h-[6rem] max-h-[6rem] shadow-md rounded-lg"
-        :class="{'favorited': is_fav}"
-    >
-        <slot/>
+        :class="{ 'favorited': is_fav }">
+        <slot />
         <span class="songs__order font-bold max-w-[3rem] w-[3rem]">
             <slot name="order" />
         </span>
         <div class="songs__image w-[4rem] h-[4rem] overflow-hidden rounded">
-            <slot name="image"/>
+            <slot name="image" />
         </div>
         <div class="songs__play text-gr text-3xl cursor-pointer">
             <slot name="play-button">
-                
+
             </slot>
         </div>
         <span class="songs__name font-bold max-w-[10rem] w-[10rem] text-center">
@@ -23,11 +22,8 @@
         <span class="songs__duration text-gr max-w-[4.5rem] w-[4.5rem] hidden lg:inline md:inline sm:inline">
             <slot name="duration" />
         </span>
-        <div class="songs__fav text-gr text-3xl cursor-pointer"
-            @click="toggle_fav"
-        >
+        <div class="songs__fav text-gr text-3xl cursor-pointer" @click="toggle_fav">
             <slot name="fav">
-                <ion-icon name="heart"></ion-icon>
             </slot>
         </div>
     </li>
@@ -45,13 +41,16 @@ const toggle_fav = (): void => {
 
 <style lang="scss" scoped>
 .songs {
-    &__order, &__duration {
+
+    &__order,
+    &__duration {
         overflow: hidden;
         text-overflow: ellipsis;
         white-space: nowrap;
     }
 
-    &__name, &__artists {
+    &__name,
+    &__artists {
         display: -webkit-box;
         -webkit-line-clamp: 2;
         -webkit-box-orient: vertical;
@@ -59,25 +58,26 @@ const toggle_fav = (): void => {
     }
 
     &__fav {
-        transition: all 0.2s cubic-bezier(.49,.26,.19,.37);
+        transition: all 0.2s cubic-bezier(.49, .26, .19, .37);
     }
 
-    &__item.favorited > &__fav {
+    &__item.favorited>&__fav {
         @apply text-red-400;
     }
 
     &__item.playing {
         @apply border-2 border-green-400;
         box-shadow: 0 0 1rem rgb(74, 222, 128, 1),
-                    0 0 2rem rgba(74, 222, 128, 1),
-                    0 0 4rem rgba(74, 222, 128, 1);
+            0 0 2rem rgba(74, 222, 128, 1),
+            0 0 4rem rgba(74, 222, 128, 1);
         backdrop-filter: blur(1rem);
-        animation: hue 5s cubic-bezier(.49,.26,.19,.37) infinite alternate;
+        animation: hue 5s cubic-bezier(.49, .26, .19, .37) infinite alternate;
 
         @keyframes hue {
             0% {
                 filter: hue-rotate(0deg);
             }
+
             100% {
                 filter: hue-rotate(360deg);
             }
